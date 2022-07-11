9c5f5a0ddaaeb79e12f07156510dd02ece22a450
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-07-11 16:46:11 +0200

    create component JobList and PropType + CSS

--- hyrule-jobs/src/App.vue
+++ hyrule-jobs/src/App.vue
@@ -1,14 +1,55 @@
 <template>
   <div class="app">
-    <h1>Hello world!</h1>
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import JobList from "./components/JobList.vue";
+import Job from "./types/Job";
 
 export default defineComponent({
   name: "App",
+  components: {
+    JobList,
+  },
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death montain",
+        salary: 200,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 21000,
+        id: "3",
+      },
+      {
+        title: "fisherman",
+        location: "lake hylia",
+        salary: 43000,
+        id: "4",
+      },
+      {
+        title: "prison guard",
+        location: "lantin",
+        salary: 100000,
+        id: "5",
+      },
+    ]);
+
+    return { jobs };
+  },
 });
 </script>
 
