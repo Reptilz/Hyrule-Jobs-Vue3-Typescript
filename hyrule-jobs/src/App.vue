d986f7b143096e49ff6d2bc9af5a629866c5d04c
Morlet <34801852+Reptilz@users.noreply.github.com>
2022-07-11 14:06:06 +0200

    installation environnement

---
+++ hyrule-jobs/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <h1>Hello world!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style></style>
